d5d11fe04f8a75644dbdeb82fb701228741ba0ef
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-22 20:45:38 +0900

    practice

--- js/main.js
+++ js/main.js
@@ -1,62 +1,21 @@
-function Person(firstName, lastName, age, gender, interest) {
-  this.name = {
-    firstName: firstName,
-    lastName: lastName
-  };
-  this.age = age;
-  this.gender = gender;
-  this.interest = interest;
-  //   console.log(this.gender);
-  this.bio = function() {
-    var str =
-      this.name.firstName +
-      " " +
-      this.name.lastName +
-      " is " +
-      this.age +
-      " years old.";
-    if (gender === "male") str += " He likes ";
-    else if (gender === "female") str += " She likes ";
-    else str += " It likes ";
-    var interests = interest.length;
-    switch (interests) {
-      case 1:
-        str += this.interest[0];
-        break;
-      case 2:
-        str += this.interest[0] + " and " + this.interest[1];
-        break;
-      default:
-        str += "much more";
-        break;
-    }
-    str += ".";
-    alert(str);
+var obj = {
+    "Apple": "Steve Jobs",
+    "Microsoft": "Bill Gates"
+};
 
-    // alert(
-    //   this.name.firstName +
-    //     " " +
-    //     this.name.lastName +
-    //     " is " +
-    //     this.age +
-    //     " years old. He likes " +
-    //     this.interest[0] +
-    //     " and " +
-    //     this.interest[1] +
-    //     "."
-    // );
-  };
-  Person.prototype.greeting = function() {
-    alert("Hi! I'm " + this.name.firstName + ".");
-  };
-}
+// var key = "Apple";
+// if (obj.hasOwnProperty(key)) {
+//     delete obj[key];
+// }
+var _obj = obj;
+obj["Oracle"] = "Larry Ellison";
+console.log(_obj);
 
-var person1 = new Person("Bob", "Smith", 32, "male", [
-  "music",
-  "skiing",
-  "Battle"
-]);
-console.log(person1["age"]);
-console.log(person1.interest[1]);
-console.log(person1.gender);
-console.log(person1.bio());
+Object.freeze(obj);
+obj.Apple = "John Sculley";
+console.log(obj);
+
+o["function"] = function(){
+    console.log("Well done!")
+};
+o.function();

16518129992e8f8c52cb431b6f52023cd14da987
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-22 17:47:11 +0900

    tutorial

--- js/main.js
+++ js/main.js
@@ -1,13 +1,62 @@
-function mixin(target, source) {
-  for (var property in source) {
-    if (source.hasOwnProperty(property)) {
-      target[property] = source[property];
+function Person(firstName, lastName, age, gender, interest) {
+  this.name = {
+    firstName: firstName,
+    lastName: lastName
+  };
+  this.age = age;
+  this.gender = gender;
+  this.interest = interest;
+  //   console.log(this.gender);
+  this.bio = function() {
+    var str =
+      this.name.firstName +
+      " " +
+      this.name.lastName +
+      " is " +
+      this.age +
+      " years old.";
+    if (gender === "male") str += " He likes ";
+    else if (gender === "female") str += " She likes ";
+    else str += " It likes ";
+    var interests = interest.length;
+    switch (interests) {
+      case 1:
+        str += this.interest[0];
+        break;
+      case 2:
+        str += this.interest[0] + " and " + this.interest[1];
+        break;
+      default:
+        str += "much more";
+        break;
     }
-  }
-  return target;
+    str += ".";
+    alert(str);
+
+    // alert(
+    //   this.name.firstName +
+    //     " " +
+    //     this.name.lastName +
+    //     " is " +
+    //     this.age +
+    //     " years old. He likes " +
+    //     this.interest[0] +
+    //     " and " +
+    //     this.interest[1] +
+    //     "."
+    // );
+  };
+  Person.prototype.greeting = function() {
+    alert("Hi! I'm " + this.name.firstName + ".");
+  };
 }
 
-var obj1 = { a: 1, b: 2 };
-var obj2 = { b: 3, c: 4 };
-var obj = mixin(obj1, obj2);
-console.log(obj);
+var person1 = new Person("Bob", "Smith", 32, "male", [
+  "music",
+  "skiing",
+  "Battle"
+]);
+console.log(person1["age"]);
+console.log(person1.interest[1]);
+console.log(person1.gender);
+console.log(person1.bio());

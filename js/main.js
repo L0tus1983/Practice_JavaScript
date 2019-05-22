1fe2c7d958eab7a1d90e9aaba4b4f2fe19058d00
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-23 07:24:05 +0900

    array

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
-var obj = {
-    "Apple": "Steve Jobs",
-    "Microsoft": "Bill Gates"
-};
+function compareFunc(key){
+    return function(a, b){
+        return a[key] - b[key];
+    }
+}
 
-// var key = "Apple";
-// if (obj.hasOwnProperty(key)) {
-//     delete obj[key];
-// }
-var _obj = obj;
-obj["Oracle"] = "Larry Ellison";
-console.log(_obj);
+var persons = [{name:"Tom", age:17}, {name:"Huck", age:18},{name:"Becky", age:16}];
+persons.sort(compareFunc('name'));
+console.log(persons);
 
-Object.freeze(obj);
-obj.Apple = "John Sculley";
-console.log(obj);
-
-o["function"] = function(){
-    console.log("Well done!")
-};
-o.function();
+var a = ["A", "B", "C"];
+var b = a.join();
+console.log(b);
+// 参照渡しコピー（シャローコピー）
+var c = a.concat("D",["E",["F", "G"]]);
+console.log(c);
+ var a = ["A", "B", "C", "D", "E"];
+ var b = a.slice(1, 3);
+ console.log(b);
+ var a = ["A", "B", "C", "C", "E"];
+ var c = a.indexOf('C');
+var d = a.lastIndexOf('C');
+console.log(c);
+console.log(d);
+console.log(c.toString());
+console.log(a.toString());
+var  a = [1,2,3,4,5];
+var sum = 0;
+a.forEach((value) => sum += value);
+console.log(sum);
+a.forEach((v, i, a) => a[i] = v * v);
+console.log(a);
+var sq = a.map(Math.sqrt);
+console.log(sq);
+a = [1,2,3,4,5];
+var b = a.map(x => 2 * x);
+console.log(b);

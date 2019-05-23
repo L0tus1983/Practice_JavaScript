e0a6823553c96605d9c2ad1bd0436e5c18c59aa9
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-24 07:24:14 +0900

    molesmash

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
-var num1 = 10;
-var num2 = 20;
-console.log(num1, num2);
-//
-[num1, num2] = [num2, num1];
-console.log(num1, num2);
+// MoleGame
+function makeMoles(nx, ny) {
+    var moleState = new Map()
+    const W = 50, SPACE = 10
+    for (let i = 0; i < nx; i++) {
+        for(let j = 0;j <ny; j++){
+            let element = document.createElement('div')
+            element.style.width = W + "px"
+            element.style.height = W + "px"
+            element.style.backgrou = "url(./res/bigger.gif)"
+            element.style.position = "absolute"
+            element.style.opacity = 0.2
+            element.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease"
+            document.body.appendChild(element)
+            element.style.left = SPACE + i * ( W + SPACE) + "px"
+            element.style.top = 2 * SPACE + j * (W + SPACE) + "px"
+            moleState.set(element,{x:i, y:j, opacity: 0.2})
+            element.onclick = function clickEventHandler(e) {
+                var element = e.currentTarget
+                var state = moleState.get(element)
+                if (state.opacity >= 0.5) {
+                    document.body.removeChild(element)
+                    moleState.delete(element)
+                }
+            }
+        }
+    } 
+    return moleState
+}
+
+window.onload = function () {
+    const TIME_INTERVAL = 1500, DISPLAY_TIME = 1050
+    let molesState = makeMoles(7, 4)
+}

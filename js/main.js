753d54309ea773b22d19280dc7bf9b765afb5766
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-23 17:47:08 +0900

    practice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var a = { 0: "A", 1: "B", 2: "C", length: 3 };
-Array.prototype.join.call(a, ",");
-console.log(Array.prototype.push.call(a, "D"));
-console.log(a);
-Array.prototype.slice.call(a, 0);
+var num1 = 10;
+var num2 = 20;
+console.log(num1, num2);
+[num1, num2] = [num2, num1];
+console.log(num1, num2);

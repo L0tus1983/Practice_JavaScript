d42d323611b69bbee252bd91859a6c05f91da84e
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-24 17:52:40 +0900

    practice

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,8 +1,11 @@
-"use strict";
-
-function permutation(a) {
-  if (!(a instanceof Array)) {
-    throw new Error(a + " is not an array!");
-  }
-}
-permutation("ABC");
+window.onload = function() {
+  let h1 = document.querySelector("h1");
+  let para = document.querySelector("p");
+  let list = document.querySelector("li");
+  h1.style.color = "#0000ff";
+  h1.style.backgroundColor = "#ffff00";
+  h1.style.border = "1px solid black";
+  para.style.color = "red";
+  para.style.textDecoration = "underline";
+  list.style.textDecoration = "underline";
+};

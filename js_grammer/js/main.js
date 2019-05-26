94d3988166d344ce6930cd9e78e8f4e172ff2a99
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-27 06:19:41 +0900

    practice

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,11 +1,3 @@
-window.onload = function() {
-  let h1 = document.querySelector("h1");
-  let para = document.querySelector("p");
-  let list = document.querySelector("li");
-  h1.style.color = "#0000ff";
-  h1.style.backgroundColor = "#ffff00";
-  h1.style.border = "1px solid black";
-  para.style.color = "red";
-  para.style.textDecoration = "underline";
-  list.style.textDecoration = "underline";
-};
+"use strict"
+let reg = /abc/gi
+console.log(reg.flags)

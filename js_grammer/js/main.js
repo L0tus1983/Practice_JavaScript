393858365eea2ca2bc3722c912c7e55ae9731b12
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-28 08:47:21 +0900

    test

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,3 +1,15 @@
-"use strict"
-let reg = /abc/gi
-console.log(reg.flags)
+"use strict";
+window.onload = function () {
+  let userCheck = prompt('Who\'s there?')
+  let adminPass;
+  if (userCheck === 'Admin') {
+    adminPass = prompt('Password?', '')
+    if (adminPass === 'The') {
+
+    }
+  } else if (userCheck === null) {
+    alert('Canceled')
+  } else {
+    alert('I don\'t know you')
+  }
+}

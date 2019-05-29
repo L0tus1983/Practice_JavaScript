d653d6ed5095997ad14ca7d8db02d5a07bc6ddfa
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-29 17:26:29 +0900

    practice!

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,26 +1,5 @@
 "use strict";
 
-window.onload = function() {
-  document.addEventListener(
-    "keydown",
-    function(event) {
-      let prop = [
-        "altKey",
-        "ctrlKey",
-        "shiftKey",
-        "metaKey",
-        "key",
-        "code",
-        "keyCode"
-      ];
-      let description = "";
-      let display = document.getElementById("display");
-      for (let item of prop) {
-        description += "<br>" + prop[item] + " : " + event[prop[item]];
-      }
-      description += " -> " + String.fromCharCode(event.keyCode);
-      display.innerHTML = description;
-    },
-    false
-  );
-};
+let str = {};
+str.test = 5;
+alert(str.test);

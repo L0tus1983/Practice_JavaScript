12fba667fb83dbab58567ad3c7a609aad5eda5e0
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-30 15:58:28 +0900

    ajax

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,6 +1,16 @@
-"use strict";
-
-window.onload = function() {
-  console.log(1e-3 === 1 / 1000);
-  console.log(1.23e-6 === 1.23 / 1000000);
-};
+function getUserInfo(userId) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://api.github.com/users/${userId}`);
+  request.addEventListener("load", event => {
+    if (event.target.status !== 200) {
+      console.error(`${event.target.status}: ${event.target.statusText}`);
+      return;
+    }
+    console.log(event.target.status);
+    console.log(event.target.responseText);
+  });
+  request.addEventListener("error", () => {
+    console.error("Network Error");
+  });
+  request.send();
+}

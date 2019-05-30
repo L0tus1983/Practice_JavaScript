d8a468fc64a91cf9836b3d26ea0e83cf245dad5a
Tanaka <l0tus.yang.dage@gmail.com>
2019-05-31 05:09:37 +0900

    test

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,5 +1,16 @@
 "use strict";
 
-let str = {};
-str.test = 5;
-alert(str.test);
+window.onload = function() {
+  //
+  let req = new XMLHttpRequest();
+  //
+  req.onreadystatechange = function() {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        document.getElementById("view").innerHTML = req.responseText;
+      }
+    }
+  };
+  req.open("GET", "http://codecamp.lesson.codecamp.jp/javascript/ajax.txt");
+  req.send(null);
+};

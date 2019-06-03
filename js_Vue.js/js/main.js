2efd47afa6276794d4c98a9037c400f784f82b40
Tanaka <l0tus.yang.dage@gmail.com>
2019-06-03 16:06:41 +0900

    vue

---
+++ js_Vue.js/js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// let vm = new Vue({
+//   el: "#demo",
+//   data: {
+//     firstName: "Foo",
+//     lastName: "Bar",
+//     fullName: "Foo Bar"
+//   },
+//   watch: {
+//     firstName: function(val) {
+//       this.fullName = val + " " + this.lastName;
+//     },
+//     lastName: function(val) {
+//       this.fullName = this.firstName + " " + val;
+//     }
+//   }
+// });
+
+let vm = new Vue({
+  el: "#demo",
+  data: {
+    firstName: "Foo",
+    lastName: "Bar"
+  },
+  computed: {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+});

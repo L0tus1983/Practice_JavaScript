3c42431411f92189e7c08e615d96c52641b04f0e
Tanaka <l0tus.yang.dage@gmail.com>
2019-06-07 07:31:39 +0900

    data binding

--- js_Vue.js/js/main.js
+++ js_Vue.js/js/main.js
@@ -1,31 +1,18 @@
 "use strict";
 
-// let vm = new Vue({
-//   el: "#demo",
-//   data: {
-//     firstName: "Foo",
-//     lastName: "Bar",
-//     fullName: "Foo Bar"
-//   },
-//   watch: {
-//     firstName: function(val) {
-//       this.fullName = val + " " + this.lastName;
-//     },
-//     lastName: function(val) {
-//       this.fullName = this.firstName + " " + val;
-//     }
-//   }
-// });
+let app = new Vue({
+  el: "#app",
 
-let vm = new Vue({
-  el: "#demo",
-  data: {
-    firstName: "Foo",
-    lastName: "Bar"
-  },
-  computed: {
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
+  data() {
+    return {
+      classObj: {
+        child: true,
+        isActive: false
+      },
+      styleObj: {
+        color: "blue",
+        backgroundColor: "gray"
+      }
+    };
   }
 });

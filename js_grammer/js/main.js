e1c3e128cb14d7a141218e1def451edf55f4b975
Tanaka <l0tus.yang.dage@gmail.com>
2019-06-07 23:47:15 +0900

    tekitou

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,25 +1,6 @@
 "use strict";
 
 window.onload = function() {
-<<<<<<< HEAD
-  let header = document.getElementById("header");
-  let deg = 0;
-  let rotateHeader = () => {
-    console.log("ここに来てない？");
-    deg += 6;
-    deg %= 360;
-    if ((0 <= deg && deg < 90) || (270 <= deg && deg < 360)) {
-      header.className = "face";
-    } else {
-      header.className = "back";
-    }
-    header.style.transform = `rotateX(${deg}deg)`;
-    //
-  };
-
-  setInterval(rotateHeader, 20);
-=======
   let select = document.getElementById("aaa");
   select.remove(0);
->>>>>>> 7bde996e5efeb8ee6242ebb9a80dce78605ed07f
 };

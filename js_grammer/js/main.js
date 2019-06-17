6ae088633c3513673202971d14e767ea3ef1df80
Tanaka <l0tus.yang.dage@gmail.com>
2019-06-17 17:42:53 +0900

    jquery

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,13 +1,43 @@
-"use strict";
+'use strict'
 
-function func() {
-  console.log("aaa");
+function greet() {
+  let greetingObj
+  let greetingWord
+  let targetName = document.getElementById('name').value
+  let hour = new Date().getHours()
+
+  if (6 <= hour && hour < 12) {
+    greetingWord = 'Good morning'
+  } else if (12 <= hour && hour < 18) {
+    greetingWord = 'Hello'
+  } else {
+    greetingWord = 'Good evening'
+  }
+
+  greetingObj = {
+    greetingWord: greetingWord,
+    targetName: targetName
+  }
+
+  return greetingObj
 }
-function func2() {
-  console.log("bbb");
-}
 
-let p = document.getElementById("abcd");
+document
+  .getElementById('greeting-button')
+  .addEventListener('click', function() {
+    let greetingObj = greet()
 
-p.addEventListener("click", func, false);
-p.addEventListener("click", func2, false);
+    document.getElementById('box').innerHTML = `${greetingObj.greetingWord}、${
+      greetingObj.targetName
+    }.`
+  })
+
+document
+  .getElementById('cheerful-button')
+  .addEventListener('click', function() {
+    let greetingObj = greet()
+
+    document.getElementById('box').innerHTML = `${
+      greetingObj.greetingWord
+    }！、${greetingObj.targetName}！！`
+  })

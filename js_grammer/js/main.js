b6f1967e23c1ee5405d884a5b24e2e5451fb294c
Tanaka <l0tus.yang.dage@gmail.com>
2019-06-21 20:59:03 +0900

    js

--- js_grammer/js/main.js
+++ js_grammer/js/main.js
@@ -1,25 +1,13 @@
 'use strict'
 
-// セレクタに該当するタブを表示
-function showTab(selector) {
-  // selectorの中身
-  console.log(selector)
-
-  $('.tabs-menu div').removeClass('active')
-  $('.tabs-menu ' + selector).addClass('active')
-  
-  $('.tabs-content > div').hide()
-  $(selector).show()
-  
-}
-
 $(() => {
-  // 初期状態として１番目のタブを表示
-  showTab('#tab-menu-a')
-
-  $('.tabs-menu div').click(function () {
-    let selector = $(this).attr('id')
-    showTab(selector)
-    return false
+  $('.animated').waypoint({
+    handler: function (direction) {
+      if (direction === 'down') {
+        $(this.element).addClass('fadeInUp')
+        this.destroy()
+      }
+    },
+    offset: '50%'
   })
 })
